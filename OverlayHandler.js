bdc797d97e672efd06957c05ba5de07c74226b9d
Albert <webbhelp_@hotmail.com>
2016-11-29 11:08:41 +0100

    Add Overlayhandler

---
+++ OverlayHandler.js
@@ -0,0 +1,61 @@
+(function () {
+    'use strict';
+    /*===========================
+    OverlayHandler
+    ===========================*/
+    var OverlayHandler = {
+        selector: null,
+        callbacks: {
+            onClose: {}
+        },
+        initialize: function($selector) {
+            OverlayHandler.selector = $selector;
+        },
+        show: function(showLoader) {
+            var showLoader = showLoader || false;
+            OverlayHandler.selector.addClass('active');
+            if (loading) {
+                OverlayHandler.selector.addClass('loading');
+            }
+        },
+        hide: function() {
+            OverlayHandler.selector.removeClass('active loading');
+            var callbacks = OverlayHandler.callbacks;
+            for (var i in callbacks.onClose) {
+                callbacks.onClose[i](OverlayHandler.selector);
+            }
+        },
+        showLoader: function() {
+            OverlayHandler.selector.addClass('loading');
+        },
+        hideLoader: function() {
+            OverlayHandler.selector.removeClass('loading');
+        },
+        showLoading: function() { OverlayHandler.showLoader() },
+        hideLoading: function() { OverlayHandler.hideLoader() },
+        onClose: function(callback) {
+            if (typeof callback == 'function') {
+                OverlayHandler.callbacks.onClose.push(callback);
+            }
+        },
+        setSelector: function($selector) {
+            OverlayHandler.selector = $selector;
+        },
+        getSelector: function() {
+            return OverlayHandler.selector;
+        }
+    };
+    window.OverlayHandler = OverlayHandler;
+})();
+
+/*===========================
+OverlayHandler AMD Export
+===========================*/
+if (typeof(module) !== 'undefined') {
+    module.exports = window.OverlayHandler;
+} else if (typeof define === 'function' && define.amd) {
+    define([], function () {
+        'use strict';
+        return window.OverlayHandler;
+    });
+}
